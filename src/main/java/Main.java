32272965a8b0f6c134ba5b3e058c6a1814187e1e
Maciej Rusek <m_rusek@vp.pl>
2017-04-25 13:56:39 +0200

    add rectangle class and generateRectanglePoint method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,15 @@
+import java.awt.*;
+
 /**
  * Created by maciek on 4/25/17.
  */
 public class Main {
 
     public static void main(String []args){
+        Rectangle rectangle = new Rectangle();
 
+        Point p = new Point(0,0);
+
+        rectangle.generateRectanglePoints(p, 1);
     }
 }
---
+++ src/main/java/Rectangle.java
@@ -0,0 +1,35 @@
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by maciek on 4/25/17.
+ */
+public class Rectangle {
+    public Rectangle(){
+
+    }
+
+    public ArrayList generateRectanglePoints(Point pointOfEngagment, int movement){
+        ArrayList<Point> pointsList = new ArrayList<Point>();
+        pointsList.add(pointOfEngagment);
+        int xCoordinate = (int) pointOfEngagment.getX();
+        int yCoordinate = (int) pointOfEngagment.getY();
+
+        Point p1 = new Point(xCoordinate + movement, yCoordinate);
+        Point p2 = new Point(xCoordinate, yCoordinate + movement);
+        Point p3 = new Point(xCoordinate + movement, yCoordinate + movement);
+
+        pointsList.add(pointOfEngagment);
+        pointsList.add(p1);
+        pointsList.add(p2);
+        pointsList.add(p3);
+
+        System.out.println(pointOfEngagment.toString());
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
+        System.out.println(p3.toString());
+
+        return pointsList;
+    }
+}
